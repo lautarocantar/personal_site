63c77555aca56507b1814c75a1ac5367c31393f1
Lautaro Cantar <lautaro.cantar.ar@gmail.com>
2018-11-21 17:13:22 -0300

    v5

--- build_site.R
+++ build_site.R
@@ -1,6 +1,7 @@
 # Set wd
 
-setwd("/Users/lauticantar/Google Drive/Lauti_website/")
+# setwd("/Users/lauticantar/Google Drive/Lauti_website/")
+setwd("/Users/lauti/github/personal_site/")
 
 # reder your sweet site
 rmarkdown::render_site()
